031d34e450efb1c06f70df4ad17b5db417b0c793
neiljoseph717 <neiljosephdelrosario@gmail.com>
2025-07-16 03:32:49 +0800

    Add MakeCents/app/src/main/java/com/makecents/app/ui/theme/Color.kt 1752607969996

---
+++ MakeCents/app/src/main/java/com/makecents/app/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.makecents.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Make Cents Brand Colors - Blue gradient theme
+val PrimaryBlue = Color(0xFF4285F4)
+val SecondaryBlue = Color(0xFF6C5CE7)
+val LightBlue = Color(0xFF74B9FF)
+val DarkBlue = Color(0xFF2D3436)
+val BackgroundLight = Color(0xFFF8F9FA)
+val TextDark = Color(0xFF2D3436)
+val TextLight = Color(0xFF636E72)
+val White = Color(0xFFFFFFFF)
+val GradientStart = Color(0xFF6C5CE7)
+val GradientEnd = Color(0xFF4285F4)
